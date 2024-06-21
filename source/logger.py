444f1106ac5ec4cc7ace2311c7b88ac4287f804a
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-06-21 15:17:28 +0530

    this logging file will have track of error or custom exception occur during run time execution of code by inter preters

---
+++ source/logger.py
@@ -0,0 +1,27 @@
+#this file we used to track the error or custom exception occur during runtime execution of code by interpreters
+#and tracked error saving into text file with.log extension and all text file saving into logs directory
+
+#so importing all the important library which is used to create logging module
+import logging
+import os,sys
+from datetime import datetime
+
+
+
+#creating log folder that contain logging file in it
+folder_name = 'LOGS'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name,exist_ok=True)
+
+#now creating log_file filepath
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") #now and strftime is a built in method of datetime class
+filename = f"LOGS_FILE_{timestamp}.log"
+log_file_path = os.path.join(folder_name,filename)
+
+
+#now creating an object of basicConfig class of logging library
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s -%(lineno)d- %(message)s' # Log message format
+)
