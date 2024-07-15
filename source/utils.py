f27e2f0fec4fbe6e846fef9e4a6c036fd7c5fe9e
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-07-16 01:17:07 +0530

    this is utils file provide functionality to entire application

--- source/utils.py
+++ source/utils.py
@@ -10,8 +10,8 @@ def Save_object(filepath,object):
     logging.info('Here in Utils we r Creating Save object Function will Save model and preprocessor files to artifacts Folder')
     try:
         logging.info('Now Checking Filepath Exist or not')
-        if not os.path.exists(filepath):
-            os.makedirs(filepath,exist_ok=True)
+        if not os.path.exists('artifacts'):
+            os.makedirs('artifacts',exist_ok=True)
         with open(filepath,'wb') as file:
             dill.dump(object,file)
         logging.info('Object Save Into Artfacts Folder')
